de553cd6e528d8124a8f29a1e0f10a6a440c4ae0
Edyta Rozczypała <93035594+edytaroz@users.noreply.github.com>
2023-06-01 20:06:51 +0200

    Averaging networks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import neural_networks_test
+import average_network_test
 if __name__ == '__main__':
-    neural_networks_test.nnclassifier()
-
+    #neural_networks_test.nnclassifier()
+    average_network_test.test()
